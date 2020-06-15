957a01a851af8c1ecb82c65c99a698698d0d0ca8
JOBANJIT SINGH <jobanjitpannu@outlook.com>
2020-06-15 12:04:54 -0400

    Added names+message styler

--- js/index.js
+++ js/index.js
@@ -1,12 +1,3 @@
-'use strict'
-
-const socket = io()
-
-// Send a message to say that I've connected
-socket.emit('newuser', {user: 'Grace Hopper'})
-
-// Event listener, waiting for an incoming "newuser"
-socket.on('newuser', (data) => console.log(`${data.user} has connected!`))
 
 
 // Listen for the 'submit' of a form
@@ -17,19 +8,69 @@ socket.on('newuser', (data) => console.log(`${data.user} has connected!`))
 
 const $msgForm = document.getElementById('sendMsg')
 const $msgList = document.getElementById('messages')
+const $userNameForm = document.getElementById('userdetails')
+
+const socket = io()
+
+let $username = 'Guest';
+let $sentmsgtime = 0;
+
+// Function to get user name
+$userNameForm.addEventListener('submit',(event)=>{
+	event.preventDefault()
+
+	//enable msg box and chats only after entering username
+	$msgForm.style.display='block';
+	$msgList.style.display='flex';
+	$userNameForm.style.display='none';
+	$username = event.currentTarget.username.value;
+
+	// Send a message to say that I've connected
+	socket.emit('newuser', {user: `${$username} joined the chat`})
+})
+
+// Event listener, waiting for an incoming "newuser"
+socket.on('newuser', (data) => {
+	const newMsg = document.createElement('li')
+	$msgList.appendChild(newMsg)
+	newMsg.textContent = data.user
+	newMsg.classList.add("userjoined");
+})
 
 
 $msgForm.addEventListener('submit', (event) => {
 	event.preventDefault()
 
-	socket.emit('chatmsg', {msg: event.currentTarget.txt.value})
+	// To Add sender msg in the message block
+	const newMsg = document.createElement('li')
+	$msgList.appendChild(newMsg)
+	newMsg.textContent = event.currentTarget.txt.value;
+	newMsg.classList.add("messagesent");
+
+	//get time of msg sent
+	getDateTime();
+	console.log($sentmsgtime);
+
+
+	// Emit message to others
+	socket.emit('chatmsg', {msg: event.currentTarget.txt.value, sentdatetime: $sentmsgtime})
 	event.currentTarget.txt.value = ''
 })
 
 
+// To receive msg
 socket.on('chatmsg', (data) => {
 	const newMsg = document.createElement('li')
 	$msgList.appendChild(newMsg)
-
-	newMsg.textContent = data.msg
+	newMsg.classList.add("messagereceive");
+	//newMsg.textContent = data.msg;
+	newMsg.innerHTML = data.msg + "<b class='senderDetails'> - by " + $username + " at "+ data.sentdatetime +"</b>";
 })
+
+//referenced code from https://tecadmin.net/get-current-date-time-javascript/#:~:text=Current%20Time%20in%20JavaScript,%3Ai%3As%E2%80%9D%20format.&text=var%20today%20%3D%20new%20Date()%3B,()%20%2B%20%22%3A%22%20%2B
+function getDateTime(){
+	var today = new Date();
+	var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	$sentmsgtime = date+' '+time;
+}
