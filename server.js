aabd61c9d7fd4f230be3f055490a18040128d865
JOBANJITSINGH <46654719+n01302646@users.noreply.github.com>
2020-06-30 09:53:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express  = require('express')
 const app      = express()
-const port     = 3000
+const port     = process.env.PORT || 3000
 const server   = app.listen(port, () => {console.log(`Server running at http://localhost:${port}`)})
-const io 			 = require('socket.io').listen(server)
+const io       = require('socket.io').listen(server)
 
 
 app.get('/', (req, res) => {
